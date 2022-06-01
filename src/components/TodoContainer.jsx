c1a045868c4c66ba0ed2462f3b59a18065d96375
Assad Isah <nottherealalanturing@gmail.com>
2022-06-01 09:59:02 +0100

    feat: persist changes to local storage if there's a components update (add or remove todo)

--- src/components/TodoContainer.jsx
+++ src/components/TodoContainer.jsx
@@ -68,6 +68,13 @@ class TodoContainer extends React.Component {
     });
   };
 
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.todos !== this.state.todos) {
+      const temp = JSON.stringify(this.state.todos);
+      localStorage.setItem('todos', temp);
+    }
+  }
+
   render() {
     return (
       <div className="container">
